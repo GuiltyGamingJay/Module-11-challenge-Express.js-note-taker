3c5067b3264ae5f97a88d01cf819437ef065570d
GuiltyGamingJay <jerontai07@gmail.com>
2023-01-24 01:47:51 -0500

    api route folder created and linked in server.js

--- server.js
+++ server.js
@@ -8,3 +8,18 @@ const app=express();
 
 const port = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// middleware
+app.use(express.static("public"));
+
+// routes folder 
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+// server listening for requests
+app.listen(PORT, function () {
+    console.log(`App listening at http://localhost:${PORT}`);
+});
